4792971cbf3217c05f608d9db05caedbe6eff67d
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-12-29 12:46:20 +0100

    ExceptionHandler BadRequest and Conflict

--- src/main/java/com/filmFinder/FilmFinder/controller/advice/UserControllerAdvice.java
+++ src/main/java/com/filmFinder/FilmFinder/controller/advice/UserControllerAdvice.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.nio.file.AccessDeniedException;
 import java.time.LocalDateTime;
@@ -14,6 +15,29 @@ import java.util.Map;
 
 @ControllerAdvice
 public class UserControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({IllegalArgumentException.class})
+    public @ResponseBody Map<String, Object> handleInvalidInformation(IllegalArgumentException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("error", "Bad Request");
+        response.put("message", ex.getMessage());
+        return response;
+    }
+
+
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler({AccessDeniedException.class})
+    public @ResponseBody Map<String, Object> handleAccessDeniedException(AccessDeniedException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.FORBIDDEN.value());
+        response.put("error", "Access denied");
+        response.put("message", "You do not have permission to access this resource");
+        return response;
+    }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler({EntityNotFoundException.class})
@@ -26,14 +50,14 @@ public class UserControllerAdvice {
         return response;
     }
 
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    @ExceptionHandler({AccessDeniedException.class})
-    public @ResponseBody Map<String, Object> handleAccessDeniedException(AccessDeniedException ex) {
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler({ResponseStatusException.class})
+    public @ResponseBody Map<String, Object> handleConflictUsername(ResponseStatusException ex) {
         Map<String, Object> response = new HashMap<>();
         response.put("timestamp", LocalDateTime.now());
-        response.put("status", HttpStatus.FORBIDDEN.value());
-        response.put("error", "Access denied");
-        response.put("message", "You do not have permission to access this resource");
+        response.put("status", HttpStatus.CONFLICT.value());
+        response.put("error", "Already use");
+        response.put("message", ex.getMessage());
         return response;
     }
 }
